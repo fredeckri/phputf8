e6b41c07e8c941f3806989904534aa020ef9870d
fredeckri <73676607+fredeckri@users.noreply.github.com>
2021-12-08 23:18:15 -0300

    Update jsonpath.sql

--- jsonpath.sql
+++ jsonpath.sql
@@ -9,3 +9,11 @@ FOR JSON PATH;
 
 SELECT @@version as a for JSON PATH
 
+DECLARE @jsonInfo NVARCHAR(MAX)
+DECLARE @town NVARCHAR(32)
+
+SET @jsonInfo=N'{"info":{"address":[{"town":"Paris"},{"town":"London"}]}}';
+
+SET @town=JSON_VALUE(@jsonInfo,'$.info.address[0].town'); -- Paris
+SET @town=JSON_VALUE(@jsonInfo,'$.info.address[1].town'); -- London
+
